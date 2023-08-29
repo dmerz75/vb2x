67952c61ca72efd21e0f6719568b3cb913cb35a3
Dale R. Merz Jr <merz.drm@gmail.com>
2023-08-28 22:54:53 -0400

    remove polls and create

--- vbsite/run_db_migrations.sh
+++ vbsite/run_db_migrations.sh
@@ -6,13 +6,14 @@ python manage.py flush
 python manage.py syncdb
 
 python manage.py makemigrations   # edit app
+python manage.py migrate --fake lets_play
 python manage.py migrate --run-syncdb
 
-python manage.py makemigrations polls
-python manage.py sqlmigrate polls 0001
+# python manage.py makemigrations polls
+# python manage.py sqlmigrate polls 0001
 
-python manage.py makemigrations vbcourtassign
-python manage.py sqlmigrate vbcourtassign 0001
+# python manage.py makemigrations vbcourtassign
+# python manage.py sqlmigrate vbcourtassign 0001
 
 python manage.py makemigrations lets_play
 python manage.py sqlmigrate lets_play 0001
